3d16d629024f7052eabda49d1c81cf1e832229af
Dobrica Pavlinusic <dpavlin@rot13.org>
2022-05-06 19:01:43 +0200

    dump ldap.users and ldif into file, commit it

--- slapcat-list-users.pl
+++ slapcat-list-users.pl
@@ -23,14 +23,20 @@ my $regex = join('|', @cols);
 
 my $user;
 
+open(my $out, '>', 'out/ldap.users');
+open(my $ldif, '>', 'out/ldif');
+
 open(my $ldap, '-|', 'sudo -u openldap slapcat');
 while(<$ldap>) {
+	print $ldif $_;
 	chomp;
 	if ( m/($regex):(:?)\s(.+)/ ) {
 		my ($a,$need_decode,$v) = ( $1,$2, $3 );
 
 		if ( $a eq 'uid' && $user ) {
-			print join(" ", map { $user->{$_} } @cols), "\n";
+			my $line = join(" ", map { $user->{$_} } @cols) . "\n";
+			print $line;
+			print $out $line;
 			$user = undef;
 		}
 
@@ -43,3 +49,5 @@ while(<$ldap>) {
 		$user->{$a} = $v;
 	}
 }
+
+system 'git -C out diff | grep + >/dev/null && git -C out commit -m $( date +%Y-%m-%d ) -a';
