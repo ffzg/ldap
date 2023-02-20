9e53d498893c57ea15375999e51dc7bf8be4d466
FFZG admin <admin@ffzg.hr>
2023-02-20 18:33:51 +0100

    list all expired users

---
+++ ldif-expired.pl
@@ -0,0 +1,71 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use autodie;
+use MIME::Base64;
+use POSIX qw(strftime);
+use Data::Dump qw(dump);
+
+my @cols = qw(
+uid
+hrEduPersonUniqueID
+hrEduPersonExpireDate
+modifyTimestamp
+pwdChangedTime
+createTimestamp
+hrEduPersonAffiliation
+displayName
+);
+
+my $regex = join('|', @cols);
+
+my $user;
+
+my $today = strftime("%Y%m%d", localtime);
+warn "# today $today";
+
+sub check_user {
+	if (
+		! defined($user->{uid}) ||
+		$user->{hrEduPersonExpireDate} eq 'NONE' ||
+		$user->{hrEduPersonExpireDate} gt $today
+	) {
+		return 0;
+	}
+
+	my $line = join(" ", map { defined $user->{$_} ? $user->{$_} : '?' } @cols) . "\n";
+	print $line;
+	#print "# user = ",dump($user), $/;
+	$user = undef;
+	return 1;
+}
+
+my $count;
+
+open(my $ldap, '<', 'out/ldif');
+while(<$ldap>) {
+	chomp;
+
+	if ( $_ eq '' && $user ) {
+		if ( check_user ) {
+			$count++;
+			#exit 0 if $count == 10;
+		}
+	}
+
+	if ( m/($regex):(:?)\s(.+)/ ) {
+		my ($a,$need_decode,$v) = ( $1,$2, $3 );
+
+		$v = decode_base64($v) if $need_decode;
+
+		if ( $a ne 'displayName' && $v =~ s/\s+/_/g ) {
+			# nop
+		}
+
+		$user->{$a} = $v;
+	}
+}
+
+check_user;
+
+
