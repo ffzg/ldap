6cd0a6b00b5eb68cf3062468100a4931303486ed
FFZG admin <admin@ffzg.hr>
2023-02-21 11:12:46 +0100

    calculate expiration days

--- ldif-expired.pl
+++ ldif-expired.pl
@@ -3,8 +3,17 @@ use warnings;
 use strict;
 use autodie;
 use MIME::Base64;
-use POSIX qw(strftime);
+use POSIX qw(strftime mktime);
 use Data::Dump qw(dump);
+
+sub yyyymmdd_to_t {
+	my $yyyymmdd = shift;
+	return mktime( 0,0,0,
+		substr($yyyymmdd,6,2),
+		substr($yyyymmdd,4,2),
+		substr($yyyymmdd,0,4) - 1900,
+	);
+}
 
 my @cols = qw(
 uid
@@ -24,17 +33,33 @@ my $user;
 my $today = $ENV{TODAY} || strftime("%Y%m%d", localtime);
 warn "# today $today";
 
+my $today_t = yyyymmdd_to_t( $today );
+
 sub check_user {
 	if (
 		! defined($user->{uid}) ||
-		$user->{hrEduPersonExpireDate} eq 'NONE' ||
-		$user->{hrEduPersonExpireDate} gt $today
+		$user->{hrEduPersonExpireDate} eq 'NONE'
 	) {
 		return 0;
 	}
 
+	my $expire_t = yyyymmdd_to_t( $user->{hrEduPersonExpireDate} );
+
+	my $expire_in_days = ( $expire_t - $today_t ) / ( 24 * 60 * 60 );
+
+	if ( $expire_in_days == 0 ) {
+		print "XXX LOCK ",dump( $user );
+	} elsif ( $expire_in_days == 14 ) {
+		print "XXX WARN about expire ",dump( $user );
+	}
+
+	if ( $expire_in_days > 30 ) {
+		return 1;
+	}
+
 	my $line = join(" ", map { defined $user->{$_} ? $user->{$_} : '?' } @cols) . "\n";
-	print $line;
+	#print $line;
+	printf "%-3d %s", $expire_in_days, $line;
 	#print "# user = ",dump($user), $/;
 	$user = undef;
 	return 1;
